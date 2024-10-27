6f466044babd5bd87c9b98e7e2e25e8fc4c9b49f
Siraaj <siraajisaacs001@gmail.com>
2024-10-27 09:37:45 +0200

    Complete React Data Fetch

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BlogPosts from './App'; // Import the BlogPosts component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BlogPosts />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
